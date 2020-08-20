a9c0088fbc900632e23b1be2c53a5b3780301510
Jacob <realshin@gmail.com>
2020-08-20 12:02:52 -0400

    Make the filesystem patchable

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule NervesSystemRpi0.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_rpi0
+  @github_organization "verypossible"
+  @app :patchable_rpi0
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
